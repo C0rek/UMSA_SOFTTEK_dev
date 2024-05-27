74a4b55719791551898a0b185b2df0afe358eb1a
Tomas Rodriguez Zoni <tomasrodriguezzoni2412@gmail.com>
2024-05-27 18:25:40 -0300

    Cambio de proyecto a calculadora.java
    hago un nuevo proyecto con Calculadora.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,9 @@
 package main;
 
-import main.Clases.MenuCalculadora;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MenuCalculadora menu = new MenuCalculadora();
-		menu.AbrirMenu();
 	}
 
 }
